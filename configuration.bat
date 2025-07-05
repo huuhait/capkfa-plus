81a9a9a12acd2e9dfbfa35a748c19e85a4379e37
Hữu Hà <huuhadz2k@gmail.com>
2025-07-05 09:03:48 -0700

    Auto build ready to use

--- configuration.bat
+++ configuration.bat
@@ -1,4 +1,5 @@
 @echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -DCMAKE_BUILD_TYPE=Release ^
   -G Ninja ^
   -DCMAKE_MAKE_PROGRAM=ninja ^
