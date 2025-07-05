6b8366a346ded7e024d5509a0570855344041de1
Hữu Hà <huuhadz2k@gmail.com>
2025-07-05 12:13:08 -0700

    up

--- configuration.bat
+++ configuration.bat
@@ -1,34 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Default HWID value (optional)
-set "HWID="
-
-:: Parse command line arguments
-:parse_args
-if "%~1"=="" goto after_args
-
-:: Check if argument starts with --hwid=
-echo %~1 | findstr /b /c:"--hwid=" >nul
-if not errorlevel 1 (
-    set "HWID=%~1"
-    set "HWID=!HWID:~7!"
-)
-shift
-goto parse_args
-
-:after_args
-
-:: Check if HWID was provided
-if "%HWID%"=="" (
-    echo ERROR: --hwid=<value> is required
+:: Check if HWID environment variable is set
+if "!HWID!"=="" (
+    echo ERROR: HWID environment variable is required
     exit /b 1
 )
 
-:: Print the parsed HWID
-echo Using HWID: %HWID%
+echo Using HWID: !HWID!
 
-:: Run CMake with the value
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 cmake -DCMAKE_BUILD_TYPE=Release ^
@@ -37,6 +17,6 @@ cmake -DCMAKE_BUILD_TYPE=Release ^
   -DVCPKG_TARGET_TRIPLET=x64-windows-static ^
   -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ^
   -DWITH_CRT_DLL=1 ^
-  -DLOCKED_HWID="%HWID%" ^
+  -DLOCKED_HWID="!HWID!" ^
   -S . ^
   -B cmake-build-release
