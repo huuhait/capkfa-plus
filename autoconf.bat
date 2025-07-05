b781c0e61c8e4668c19276bcded6d7e7fefa2766
huuhait <huuhadz2k@gmail.com>
2025-07-05 15:49:58 +0700

    up

--- autoconf.bat
+++ autoconf.bat
@@ -1,6 +1,7 @@
 @echo off
 cmake -DCMAKE_BUILD_TYPE=Release ^
   -G Ninja ^
+  -DCMAKE_MAKE_PROGRAM=ninja ^
   -DVCPKG_TARGET_TRIPLET=x64-windows-static ^
   -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ^
   -DWITH_CRT_DLL=1 ^
